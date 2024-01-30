7b1676d05d1153d94a986b7fc7f12cf7d3400ea4
Gaurav <gaurav1999.5@gmail.com>
2024-01-30 14:41:18 +0530

    Ui changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,12 @@
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.android.library") version "7.4.2" apply false
+}
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io") // Add JitPack repository here
+    }
 }
